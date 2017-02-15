29d6533ff228fc55b545e41d0bfc229dce5a811b
Shashank S. Belvadi <sbelvadi@salesforce.com>
2017-02-15 11:30:48 -0800

    Restructuring.

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+fromfolder=$1
 tofolder=( $2 )
 filestocopy=( .cmp Controller.js Helper.js .svg )
 
 dir="/Users/sbelvadi/Documents/workspace/sbelvadi-wealth1/wealth1/"
 auradir="src/aura/"
-fromfolder=$1
 
 for foldername in "${tofolder[@]}"; do
     if [ -z "$foldername" ]; then
